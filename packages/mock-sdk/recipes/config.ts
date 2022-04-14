5bf67a17fec6f587c41a60e42cd4636a8e647d2c
Niraj Kamdar <nirajkamdar27@pm.me>
2022-04-14 18:47:38 +0530

    feat: update mock sdk

--- packages/mock-sdk/recipes/config.ts
+++ packages/mock-sdk/recipes/config.ts
@@ -3,8 +3,8 @@ import { Web3ApiClientConfig } from "@web3api/client-js";
 export function getClientConfig(
   defaultConfigs: Partial<Web3ApiClientConfig>,
 ): Partial<Web3ApiClientConfig> {
-  const env = {
-    uri: "w3://ens/rinkeby/mock.defiwrapper.eth",
+  const mainEnv = {
+    uri: "w3://ipfs/QmRicDYDFBHeyNhgyMqf19xt3T67WMALfxiAGj4M7FhGFf",
     common: {
       connection: {
         node: null,
@@ -14,6 +14,35 @@ export function getClientConfig(
     query: {},
     mutation: {},
   };
-  defaultConfigs.envs = defaultConfigs.envs ? [...defaultConfigs.envs, env] : [env];
+  const covalentEnv = {
+    uri: "w3://ens/rinkeby/covalent.account.resolvers.defiwrapper.eth",
+    query: {
+      apiKey: "ckey_910089969da7451cadf38655ede",
+      chainId: 1,
+    },
+    common: {},
+    mutation: {},
+  };
+  const tokenEnv = {
+    uri: "w3://ens/rinkeby/ethereum.token.resolvers.defiwrapper.eth",
+    query: {
+      connection: {
+        networkNameOrChainId: "MAINNET",
+      },
+    },
+    common: {},
+    mutation: {},
+  };
+  defaultConfigs.envs = defaultConfigs.envs
+    ? [...defaultConfigs.envs, mainEnv, covalentEnv, tokenEnv]
+    : [mainEnv, covalentEnv, tokenEnv];
+
+  const redirect = {
+    to: "w3://ens/rinkeby/ethereum.token.resolvers.defiwrapper.eth",
+    from: "w3://ens/ethereum.token-resolvers.defiwrapper.eth",
+  };
+  defaultConfigs.redirects = defaultConfigs.redirects
+    ? [...defaultConfigs.redirects, redirect]
+    : [redirect];
   return defaultConfigs;
 }
